3b1dc01b6c936ef253a40d4e013abef007f003c3
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-07-10 23:31:37 +0100

    Tests for examples

--- tests/test_latest.py
+++ tests/test_latest.py
@@ -1,21 +1,27 @@
 from __future__ import annotations
-from typing import Optional, Tuple, List
+from typing import Optional, Tuple, List, Type
 import jsonschema as jsc
 import pytest
-from pydantic import ValidationError
+from pydantic import ValidationError, BaseModel
 from pydantic_ome_ngff.tree import Array
 from pydantic_ome_ngff.latest.multiscales import (
     Multiscale,
+    MultiscaleAttrs,
     MultiscaleDataset,
     MultiscaleGroup,
 )
+from pydantic_ome_ngff.latest.well import Well
+from pydantic_ome_ngff.latest.plate import Plate
+from pydantic_ome_ngff.latest.imageLabel import ImageLabel
 from pydantic_ome_ngff.latest.coordinateTransformations import (
     CoordinateTransform,
     VectorScaleTransform,
     VectorTranslationTransform,
 )
 from pydantic_ome_ngff.latest.axes import Axis
-from .conftest import fetch_schemas
+from .conftest import fetch_schemas, JsonLoader
+
+loader = JsonLoader("latest")
 
 
 @pytest.fixture
@@ -338,3 +344,25 @@ def test_multiscale_group_datasets_rank(default_multiscale: Multiscale):
             attrs={"multiscales": [default_multiscale.dict()]},
             children=bad_children,
         )
+
+
+@pytest.mark.parametrize(
+    ("fname", "key", "Class"),
+    [
+        ("image-label", "image-label", ImageLabel),
+        ("plate0", "plate", Plate),
+        ("plate1", "plate", Plate),
+        ("well0", "well", Well),
+        ("well1", "well", Well),
+        (
+            "multiscales",
+            None,
+            MultiscaleAttrs,
+        ),
+    ],
+)
+def test_examples(fname: str, key: str, Class: Type[BaseModel]):
+    obj = loader.load_obj(fname)
+    if key is not None:
+        obj = obj[key]
+    Class.parse_obj(obj)
