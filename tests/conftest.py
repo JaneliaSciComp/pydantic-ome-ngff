871e10d894a584a1d6fdddabb5981ac3fb314342
Davis Bennett <davis.v.bennett@gmail.com>
2023-08-02 00:22:33 -0400

    Test models against example metadata in spec (rebased) (#15) (#17)
    * Tests for examples
    
    * Fixes from example tests
    
    Co-authored-by: Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
-from typing import Tuple, Any
+from typing import Tuple, Any, Optional, Union, List, Dict
+from pathlib import Path
+import json
+
 import requests
 
 
@@ -13,3 +16,25 @@ def fetch_schemas(version: str, schema_name: str) -> Tuple[Any, Any]:
         f"https://ngff.openmicroscopy.org/{version}/schemas/{schema_name}.schema"
     ).json()
     return base_schema, strict_schema
+
+
+FIXTURE_DIR = Path(__file__).resolve().parent / "fixtures"
+
+Jso = Optional[Union[List["Jso"], Dict[str, "Jso"], int, float, bool, str]]
+
+
+class JsonLoader:
+    def __init__(self, root: Union[str, Path]) -> None:
+        self.root = FIXTURE_DIR.joinpath(root)
+
+    def _path(self, path: Union[str, Path]) -> Path:
+        p = self.root.joinpath(path)
+        if not p.name.lower().endswith(".json"):
+            p = p.with_name(p.name + ".json")
+        return p
+
+    def load_str(self, path: Union[str, Path]) -> str:
+        return self._path(path).read_text()
+
+    def load_obj(self, path: Union[str, Path]) -> dict[str, Jso]:
+        return json.loads(self.load_str(path))
