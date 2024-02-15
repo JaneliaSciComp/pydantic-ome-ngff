192d6e0b108346d60c37ba2c0bec82399193123d
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-02-15 13:50:56 +0100

    tighten some names and type annotations

--- src/pydantic_ome_ngff/latest/transforms.py
+++ src/pydantic_ome_ngff/latest/transforms.py
@@ -10,12 +10,16 @@ class Identity(tx.Identity):
     See https://ngff.openmicroscopy.org/latest/#trafo-md
     """
 
+    ...
+
 
 class PathScale(tx.PathScale):
     """ "
     A coordinateTransform with at "path" field.
     See https://ngff.openmicroscopy.org/latest/#trafo-md
     """
+
+    ...
 
 
 class PathTranslation(tx.PathTranslation):
@@ -24,6 +28,8 @@ class PathTranslation(tx.PathTranslation):
     See https://ngff.openmicroscopy.org/latest/#trafo-md
     """
 
+    ...
+
 
 class VectorTranslation(tx.VectorTranslation):
     """
@@ -31,12 +37,16 @@ class VectorTranslation(tx.VectorTranslation):
     See https://ngff.openmicroscopy.org/latest/#trafo-md
     """
 
+    ...
+
 
 class VectorScale(tx.VectorScale):
     """
     A scale transform with a `scale` field that is a vector.
     See https://ngff.openmicroscopy.org/latest/#trafo-md
     """
+
+    ...
 
 
 Scale = Union[VectorScale, PathScale]
