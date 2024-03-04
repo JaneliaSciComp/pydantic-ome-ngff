475ecca22685321f2e32cc32829bc4ce22f2c09f
Davis Vann Bennett <davis.v.bennett@gmail.com>
2024-03-04 01:12:09 +0100

    remove extra script

--- example.py
+++
@@ -1,5 +0,0 @@
-from pydantic_ome_ngff.v04.multiscales import Group
-import zarr
-url = "https://uk1s3.embassy.ebi.ac.uk/idr/zarr/v0.4/idr0062A/6001240.zarr"
-zgroup = zarr.open(url)
-group = Group.from_zarr(zgroup)
